1e0a566fd3e68dca9ec8968a9c3b2b24fe111743
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2019-02-07 13:54:58 -0500

    add about text

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,6 +8,13 @@ class About extends Component {
       <div className="About">
         <Row>
           <h2 className='center'>About Our Shop</h2>
+          <p className='about-text'>
+            Hello! My name is Laura Anne and I’m so excited to share my art with you!
+            Traveling to interesting and lesser known places in the world
+            has shown me that God has created many different forms of art that are waiting to be shared with us all.
+            I love capturing unique elements of landscapes and cultures
+            because these are the things God has used in my life to reveal Himself to me.
+          </p>
         </Row>
         <Row>
           <div className='block-center'>
@@ -15,36 +22,14 @@ class About extends Component {
           </div>
         </Row>
         <Row>
-          <Col s={12} m={6}>
-            <h5>Section 1</h5>
+          <Col s={12}>
             <p className='about-text'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Nullam mattis risus non velit maximus elementum. 
-              Mauris dignissim consequat ante nec blandit. 
-              Maecenas convallis tellus sed metus euismod, ac luctus justo imperdiet. 
-              Vestibulum quis erat eget massa mollis efficitur. 
-              Curabitur maximus et nisi sit amet dictum. 
-              In non vehicula ipsum. 
-              Pellentesque venenatis purus et congue gravida. 
-              Maecenas auctor finibus ante, vestibulum condimentum neque congue non. 
-              Donec eleifend quam non ornare facilisis. 
-              Quisque molestie vehicula placerat.
-            </p>
-          </Col>
-          <Col s={12} m={6}>
-            <h5>Section 2</h5>
-            <p className='about-text'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Nullam mattis risus non velit maximus elementum. 
-              Mauris dignissim consequat ante nec blandit. 
-              Maecenas convallis tellus sed metus euismod, ac luctus justo imperdiet. 
-              Vestibulum quis erat eget massa mollis efficitur. 
-              Curabitur maximus et nisi sit amet dictum. 
-              In non vehicula ipsum. 
-              Pellentesque venenatis purus et congue gravida. 
-              Maecenas auctor finibus ante, vestibulum condimentum neque congue non. 
-              Donec eleifend quam non ornare facilisis. 
-              Quisque molestie vehicula placerat.
+              I live in Lexington KY with my husband David and our son Israel. 
+              Lexington is where I grew up writing music, poems, stories, and painting. 
+              I love to create, however it has not been until recently that I have shared my art with others.
+              Over the past 3 years I’ve discovered a new found love for watercolor which you will see is mainly what I use. 
+              I love creating custom pieces, illustrations for children, and prints for the home, 
+              but I’m always up for new challenges! 
             </p>
           </Col>
         </Row>
